1b7dc788866dbc9cf18ca38ba9b490fef0c3d0e3
k8hertweck <k8hertweck>
2014-09-10 15:42:12 -0500

    moved gene summary information to separate script

--- blattabacteriaGeneSummary.sh
+++ blattabacteriaGeneSummary.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+##run in home folder from blattabacteriaSetup.sh
+
+##list full paths of all input files for hyphy
+ls -d $PWD/*/*.hyphy > BlattabacteriaGenes.txt
+
+##summarize alignment results
+#grep Avg. sequence length readal.out
+#alignment variation
+#GC content $f.html (change if alignments are trimmed)
+
+##summarize tree results
+#grep for tree length
+
+#visualize results in R?
 
--- blattabacteriaSetup.sh
+++ blattabacteriaSetup.sh
@@ -35,16 +35,3 @@ for f in $FILES
 	cd ..
 
 done
-
-##list full paths of all input files for hyphy
-ls -d $PWD/*/*.hyphy > ../BlattabacteriaGenes.txt
-
-##summarize alignment results
-#grep Avg. sequence length readal.out
-#alignment variation
-#GC content $f.html (change if alignments are trimmed)
-
-##summarize tree results
-#grep for tree length
-
-#visualize results in R?
