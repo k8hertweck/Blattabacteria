dbaf19d70db61061976a8b39f45d8716882f819c
k8hertweck <k8hertweck>
2014-09-11 17:31:21 -0500

    adding summary statistics for genes and selection tests

--- blattabacteriaGeneSummary.sh
+++ blattabacteriaGeneSummary.sh
@@ -5,15 +5,29 @@
 ##list full paths of all input files for hyphy
 ls -d $PWD/*/*.hyphy > blattabacteriaGenes.txt
 
-##set up summary table and include alignment length
-egrep "filename|Alignment" readal.out | tr -d "'.#A-Za-z" | paste -d" " - - > blattabacteriaSummary.txt
+##setting up summary table
+##pull out lines with gene name and alignment length, remove extra characters, place gene name and length on same line, add headers, output to file
+egrep "filename|Alignment" readal.out | tr -d "'.#A-Za-z \t" | paste -d "\t" - - | awk 'BEGIN{print "gene\talignLength"}1' > temp
 
-#alignment variation
-#GC content $f.html (change if alignments are trimmed)
+##running loops to access stats from individual gene files
+FILES=*.fas
 
-##summarize tree results
+##GC content  
+for f in $FILES
+	do
+	##select lines with frequency of C and G in alignment
+	egrep "\(C\)|\(G\)" $f/*.phy_phyml_stats.txt | tr -d "\-= f()CG" | paste -d "\t" - - 
+	##print header and columns added together, paste to temp
+done | awk 'BEGIN{print "GC"}{print $1 + $2}' | paste temp - > temp2 
+
+
 ##tree length
-#*.phy_phyml_stats.txt has ". Parsimony:"
+for f in $FILES
+	do
+	egrep "\. Parsimony:" $f/*.phy_phyml_stats.txt | tr -d ". Parsimony:\t" 
+done | awk 'BEGIN{print "treeLength"}1' | paste temp2 - > blattabacteriaGeneSummary.txt
+
+#rm temp*
 
 #visualize results in R?
 
--- blattabacteriaSelectionSummary.sh
+++ blattabacteriaSelectionSummary.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-##run in home folder from blattabacteriaSetup.sh, adds to blattabacteriaSummary.txt
+##run in home folder from blattabacteriaSetup.sh, adds to blattabacteriaGeneSummary.txt
 
 ##QSD
-##pull out two columns: # sites positively selected, # sites negatively selected
-grep "SELECTED SITES" blattabacteriaQSD.out | cut -f3 -d " " | paste -d" " - - | tr "NO" "0"
-#join results above with blattabacteriaSummary.txt
+##pull out two columns: # sites positively selected, # sites negatively selected; add header, join with blattabacteriaGeneSummary.txt
+grep "SELECTED SITES" blattabacteriaQSD.out | cut -f3 -d " " | paste -d" " - - | tr "NO" "0" | awk 'BEGIN{print "negative\tpositive}1' | paste blattabacteriaGeneSummary.txt > temp
+
+##BranchSiteREL
+
+##BUSTED
+
+#rm <extraneous files>
