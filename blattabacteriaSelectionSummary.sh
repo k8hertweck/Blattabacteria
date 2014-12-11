d521eab7ead39e950d4e29019d29fad8cc82a01f
k8hertweck <k8hertweck@gmail.com>
2014-12-11 11:15:52 -0600

    completing QSD table summary

--- blattabacteriaSelectionSummary.sh
+++ blattabacteriaSelectionSummary.sh
@@ -2,9 +2,8 @@
 
 ##run in home folder from blattabacteriaSetup.sh, adds to blattabacteriaGeneSummary.txt
 
-##QSD
-##pull out two columns: # sites positively selected, # sites negatively selected; add header, join with blattabacteriaGeneSummary.txt
-grep "SELECTED SITES" blattabacteriaQSD.out | cut -f3 -d " " | paste -d " " - - | tr "NO" "0" | awk 'BEGIN{print "negative\tpositive}1' | paste blattabacteriaGeneSummary.txt > temp
+##QSD: pull out two columns: # sites positively selected, # sites negatively selected; add header, join with blattabacteriaGeneSummary.txt
+grep "SELECTED SITES" blattabacteriaQSD.out | cut -f3 -d " " | paste -d " " - - | sed "s/NO/0/" | awk 'BEGIN{print "QSDnegative\tQSDpositive"}1' | paste blattabacteriaGeneSummary.csv - > temp
 
 ##BranchSiteREL
 ##pull out lines referencing branches under positive selection? need examples of significant results
